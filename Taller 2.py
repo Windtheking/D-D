d7e649d1847b44efdada88fce7cad2f48860668d
Windtheking <santymendoza0106@gmail.com>
2024-09-11 19:36:51 -0500

    Update Taller 2.py

--- Taller 2.py
+++ Taller 2.py
@@ -1,9 +1,10 @@
-import pandas as pd
-import numpy as np
-import xlsxwriter as xl
-dataset = pd.read_excel(r'D:\Taller python\Ventas_Olimpica_2023.xlsx', 
+dataset = pd.read_excel(r'C:\Users\Aprendiz\Desktop\Santiago ADSO-13\Taller python\ventas.xlsx', 
                         engine ='openpyxl', 
                         header = 0, usecols = "F,G")
 
 print(dataset)
-print(type(dataset))
+#print(type(dataset))
+#lis = dataset.to_numpy().tolist()
+serie = pd.Series(dataset.to_numpy().tolist())
+print(serie)
+#print(type(serie))
